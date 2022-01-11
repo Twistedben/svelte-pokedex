d2faf0aee3796c12be9b8b005b44c2dcd940d74a
Benjamin Broestl <benbroestl@gmail.com>
2022-01-10 23:54:47 -0500

    Add some styling and input

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,8 @@
   import {pokemon as pokemons} from "../stores/pokestore";
   import PokemonCard from "../components/pokemonCard.svelte";
 
-  console.log('pokemon', $pokemons)
+  let searchTerm = "";
+  let filteredPokemon = [];
 </script>
 <svelte:head>
   <title>
@@ -12,9 +13,21 @@
 
 <h1 class="text-4xl text-center my-8">Pokedex</h1>
 
-{#each $pokemons as pokemon}
+<label class="mb-4 flex flex-col">
+  <span class="pl-4 font-semibold">
+    Search for Pokemon
+  </span>
+  <input type="search" 
+  placeholder="Search Pokemon" 
+  class="rounded-lg p-4 mx-2 focus:outline-transparent foucs:shadow-md focus:border-yellow-500 w-full border-2 border-yellow-400"
+  >
+</label>
+
+<div class="py-2 grid gap-x-2 gap-y-6 md:grid-cols-3 grid-cols-2 lg:grid-cols-4">
+  {#each $pokemons as pokemon}
   <PokemonCard pokemon={pokemon}/>
-{/each}
+  {/each}
+</div>
 <style>
 
 </style>
