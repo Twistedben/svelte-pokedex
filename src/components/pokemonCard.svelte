6d8fb12f6a914795593957b52a0a8b2dd4333ba4
eXp Realty <exprealty@BJBROESTL-MBP-EXP.attlocal.net>
2022-01-10 23:52:53 -0500

    Add some styling and input

--- src/components/pokemonCard.svelte
+++ src/components/pokemonCard.svelte
@@ -1,9 +1,7 @@
 <script>
   export let pokemon;
 </script>
-<a href={`/pokemon/${pokemon.id}`}>
-  <img src={`${pokemon.imageUrl}`} alt={pokemon.name} loading="lazy">
+<a class="p-6 bg-gray-200 shadow-sm rounded-md hover:shadow-md flex flex-col transition-all ease-in duration-300 hover:scale-105 mx-2 items-center justify-center" href={`/pokemon/${pokemon.id}`}>
+  <img class="h-40 w-40" height="" src={`${pokemon.imageUrl}`} alt={pokemon.name} loading="lazy">
+  <h2 class="uppercase text-xl">{pokemon.id}. {pokemon.name}</h2>
 </a>
-<p>
-  {pokemon.name}
-</p>
